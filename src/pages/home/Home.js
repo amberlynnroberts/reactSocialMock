c828c5c0d3277a80e7ce75c4e4f179e16566646a
Amber <amber.roberts@motorolasolutions.com>
2022-12-02 15:43:54 -0500

    added readme

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import Sidebar from "../../components/sidebar/Sidebar";
+import Topbar from "../../components/topbar/Topbar";
+
+export default function Home() {
+    return (
+        <>
+        <Topbar/>
+        <div className="homeContainer">
+            <Sidebar />
+        </div>
+        </>
+    );
+}
